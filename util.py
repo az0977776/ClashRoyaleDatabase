9e65a22d287cd627f0788fd7ad20ed9d43f4e80f
Aaron <wang.aa@husky.neu.edu>
2018-03-17 14:34:52 -0400

    util file

--- util.py
+++ util.py
@@ -8,14 +8,36 @@ connection = pymysql.connect(host = "localhost",
                             cursorclass=pymysql.cursors.DictCursor)
 
 
-def getCardsFromDeck(deck,cardName):
-    """SELECT * FROM DECK LEFT JOIN CARD on"""
+def getCardsFromDeck(deck):
+    query = """
+            SELECT ... FROM deck
+            LEFT JOIN (
+            SELECT * FROM cardsInDeck
+            LEFT JOIN
+            card ON cardsInDeck.cardName = card.name
+            ) AS c
+            ON deck.name = c.deckName
+            WHERE deckName = %s
+            """
+    cursor.execute(query,(deck))
 
 
 def addCardtoDeck(deck,card):
 
 def removeCardfromDeck(deck,card):
+    query = """
+
+            """
+
 
 def addDeck(deck):
+    query = """
+            INSERT into deck VALUES (%s)
+            """
+    cursor.execute(query,(deck))
 
 def removeDeck(deck):
+    query = """
+            DELETE * FROM deck WHERE name = %s
+            """
+    cursor.execute(query, (deck))
