61a4799ded96bcffb9b530a14d22a9dca1c3ed34
Aaron <wang.aa@husky.neu.edu>
2018-04-19 15:02:36 -0400

    better error messages

--- controller.py
+++ controller.py
@@ -3,8 +3,6 @@ import model
 from tkinter import *
 import util
 
-
-
 class Controller:
     def __init__(self, root):
         self.view = view.Window(root)
@@ -35,11 +33,14 @@ class Controller:
     def get_user_password(self):
         try:
             self.model.accessDB()
-            self.show_notif("Logged in")
+            self.show_notif("Logged in as " + self.model.username.get())
         except:
-            self.show_notif("Unable to Login")
+            self.show_notif("Unable to login with given credentials")
 
     def displayDeck(self):
+        if self.model.cur_deck.get() == "":
+            self.show_notif("Please input a deck name")
+            return;
         try:
             self.current_deck = self.model.getCurDeck()
             self.view.cards_in_deck.config(state=NORMAL)
@@ -47,21 +48,27 @@ class Controller:
             self.view.cards_in_deck.insert(END, self.current_deck)
             self.view.cards_in_deck.config(state=DISABLED)
         except:
-            self.show_notif("Unable to get cards from deck")
+            self.show_notif("Unable to get cards from deck: " + self.model.cur_deck.get())
 
     def removeDeck(self):
+        if self.model.rem_add_deck.get() == "":
+            self.show_notif("Please input a deck name")
+            return;
         try:
             self.model.removeDeck()
-            self.show_notif("Deck removed")
+            self.show_notif("Deck removed: " + self.model.rem_add_deck.get())
         except:
-            self.show_notif("Unable to remove deck")
+            self.show_notif("Unable to remove deck: " + self.model.rem_add_deck.get())
 
     def addDeck(self):
+        if self.model.rem_add_deck.get() == "":
+            self.show_notif("Please input a deck name")
+            return;
         try:
             self.model.addDeck()
-            self.show_notif("Deck added")
+            self.show_notif("Deck added: " + self.model.rem_add_deck.get())
         except:
-            self.show_notif("Unable to add deck")
+            self.show_notif("Unable to add deck: " + self.model.rem_add_deck.get())
 
     def display(self, comm):
         self.text_to_display = self.model.get_table(comm)
@@ -81,54 +88,72 @@ class Controller:
         try:
             self.display(util.showAllDecks())
         except:
-            self.show_notif("Unable to get decks")
+            self.show_notif("Unable to get all decks")
 
     def displayCards(self):
         try:
             self.display(util.showAllCards())
         except:
-            self.show_notif("Unable to get cards")
+            self.show_notif("Unable to get all cards info")
 
     def displayArenas(self):
         try:
             self.display(util.showAllArenas())
         except:
-            self.show_notif("Unable to get arenas")
+            self.show_notif("Unable to get all arenas info")
 
     def displayPlayer(self):
         try:
             self.display(util.showAllPlayers())
         except:
-            self.show_notif("Unable to get players")
+            self.show_notif("Unable to get all player info")
 
     def displayChests(self):
         try:
             self.display(util.showAllChests())
         except:
-            self.show_notif("Unable to get chests")
+            self.show_notif("Unable to get all chests info")
 
     def addCard(self):
+        if self.model.change_deck.get() == "":
+            self.show_notif("Please input a deck name")
+            return;
+        if self.model.change_card.get() == "":
+            self.show_notif("Please input a card name")
+            return;
         try:
             self.model.addCard()
-            self.show_notif("Card added")
+            self.show_notif("Added card " + self.model.change_card.get() + " to deck " + self.model.change_deck.get())
         except:
-            self.show_notif("Unable to add card")
+            self.show_notif("Unable to add card " + self.model.change_card.get() + " to deck " + self.model.change_deck.get())
 
 
     def removeCard(self):
+        if self.model.change_deck.get() == "":
+            self.show_notif("Please input a deck name")
+            return;
+        if self.model.change_card.get() == "":
+            self.show_notif("Please input a card name")
+            return;
         try:
             self.model.removeCard()
-            self.show_notif("Card removed")
+            self.show_notif("Removed card " + self.model.change_card.get() + " from deck " + self.model.change_deck.get())
         except:
-            self.show_notif("Unable to remove card")
+            self.show_notif("Unable to remove card " + self.model.change_card.get() + " from deck " + self.model.change_deck.get())
 
 
     def update_DeckName(self):
+        if self.model.oldDeckName.get() == "":
+            self.show_notif("Please input an old deck name")
+            return;
+        if self.model.newDeckName.get() == "":
+            self.show_notif("Please input a new deck name")
+            return;
         try:
             self.model.updateDeckName()
-            self.show_notif("Deck name updated")
+            self.show_notif("Deck name updated from " + self.model.oldDeckName.get() + " to " + self.model.newDeckName.get())
         except:
-            self.show_notif("Could not update deck name")
+            self.show_notif("Could not update deck name from " + self.model.oldDeckName.get() + " to " + self.model.newDeckName.get())
 
 
 if __name__ == "__main__":
